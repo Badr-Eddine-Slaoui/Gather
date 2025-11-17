1a3190c6cd6495ac3ce4495c190e3fa5ba55fd57
Badr-Eddine-Slaoui <slaoui.badr.eddine.sdv@gmail.com>
2025-11-17 18:27:41 +0100

    Add Delete Modal Template

--- templates.js
+++ templates.js
@@ -215,3 +215,15 @@ export const update_modal = (worker) => {
         </div>
     `
 }
+
+export const delete_modal = () => {
+    return `
+        <div class="w-1/3 h-[50vh] bg-white rounded-lg relative flex flex-col gap-y-20 justify-center items-center">
+            <h2 class="text-center text-[2rem] font-extrabold">Are you sure you want to delete this worker?</h2>
+            <div class="flex justify-between items-center gap-x-10">
+                <button id="delete-worker-btn" class="bg-red-500 py-3 px-5 text-[1.2rem] text-white font-bold rounded-md">Delete</button>
+                <button id="cancel-delete-worker-btn" class="bg-blue-500 py-3 px-5 text-[1.2rem] text-white font-bold rounded-md">Cancel</button>
+            </div>
+        </div>
+    `
+}
